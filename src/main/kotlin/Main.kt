e9c27cfff9297f93b4bc4d963b01bc4b90ce507d
Dmitry <boetz@yandex.ru>
2023-10-12 22:08:26 +0700

    HomeWork1_2 Fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,15 @@
+import java.lang.Math.pow
+
 fun main () {
-    val likes = 21
+    val likes = 1357
     val lastWorldVariant1 = "людям"
     val lastWorldVariant2 = "человеку"
 
-    val correctVariant = if (likes % 10 == 1) lastWorldVariant2 else lastWorldVariant1
+    val number = likes.toString().length-1
+    val splitter = pow(10.0, number.toDouble()).toInt()
+    val residue = (likes % splitter).toString().last()
+
+    val correctVariant = if (residue == '1') lastWorldVariant2 else lastWorldVariant1
 
     println("Нравится "+ likes + " "+ correctVariant)
 }
